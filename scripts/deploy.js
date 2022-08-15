1e7c2650b63d577ece4b59a62dc11972f39ec02c
eienokodoku <111244739+pastrn@users.noreply.github.com>
2022-08-15 10:03:37 +0300

    added simple deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,24 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers, upgrades } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const TokenFactory = await ethers.getContractFactory("MyToken");
+    const token = await upgrades.deployProxy(TokenFactory, [], {
+      initializer: 'initialize',
+      kind: 'uups',
+    });
+    await token.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+  console.log("First version deployed to: ", token.address);
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  const TokenFactoryV2 = await ethers.getContractFactory("MyToken2");
+  await upgrades.upgradeProxy(token.address, TokenFactoryV2)
+
+  console.log("Succesfully upgraded")
+
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
